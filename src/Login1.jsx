e1accc84518fa85ad5714eac22c1f22712965d6e
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2022-10-07 16:15:39 +0530

    16:15,07-10-2022

--- src/Login1.jsx
+++ src/Login1.jsx
@@ -1,45 +1,48 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import axios from "axios";
-import _ from "lodash"
+import _ from "lodash";
 import * as EmailValidator from "email-validator";
-import IconButton from '@mui/material/IconButton';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import IconButton from "@mui/material/IconButton";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+var validUrl = require('valid-url');
+
 
 const Login1 = () => {
   const [email, setEmail] = useState("");
   // console.log(email);
-  const [err,setErr] = useState("");
+  const [err, setErr] = useState("");
 
   useEffect(() => {
     setErr("");
-  }, [email]);
+  }, []);
 
   const functionCall = async () => {
-    if(EmailValidator.validate(email)){
-        const url = `https://signin.bindid-sandbox.io/authorize?client_id=bid_demo_acme&redirect_uri=https%3A%2F%2Fdemo.bindid-sandbox.io%2F_complete%2Facme&state=798105869&bindid_aux_link_title=More%20ways%20to%20verify&bindid_aux_link=https%3A%2F%2Fdemo.bindid-sandbox.io%2Fother_login&bindid_custom_message=Login%20to%20Acme&scope=openid%20bindid_network_info&display=page&prompt=login&response_type=code&login_hint=email%3A${(email)}`;
-        console.log(url)
-        const result = await axios
-          .get(url,{headers: { 'Content-Type': 'application/json',
-          "Access-Control-Allow-Origin": "*",
-          
-        }})
-          .then((res) => {
-            console.log(res);
-            return res;
-          })
-          .catch((err) => {
-            console.log(err);
-            return err;
-          });
-    
-          console.log("Result",result)
-    }else{
-        setErr("Invalid Email")
-    }
+    if (EmailValidator.validate(email)) {
+      const url = `https://signin.bindid-sandbox.io/authorize?client_id=bid_demo_acme&redirect_uri=https%3A%2F%2Fdemo.bindid-sandbox.io%2F_complete%2Facme&state=798105869&bindid_aux_link_title=More%20ways%20to%20verify&bindid_aux_link=https%3A%2F%2Fdemo.bindid-sandbox.io%2Fother_login&bindid_custom_message=Login%20to%20Acme&scope=openid%20bindid_network_info&display=page&prompt=login&response_type=code&login_hint=email%3A${email}`;
+      console.log(url);
+      const result = await axios
+        .get(url, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          return res;
+        })
+        .catch((err) => {
+          console.log(err);
+          return err;
+        });
 
+      console.log("Result", result);
+    } else {
+      setErr("Invalid Email");
+    }
   };
 
   return (
@@ -150,25 +153,102 @@ const Login1 = () => {
             <div className="box-root  flex-flex flex-justifyContent--center"></div>
             <div className="formbg-outer">
               <div className="formbg uk-card uk-card-hover uk-card-body  p-0">
-                {<div className="formbg-inner padding-horizontal--48">
+                {
+                  <div className="formbg-inner padding-horizontal--48">
 
 
 
-                <IconButton 
-                onClick={() => {
-                  const query = new URLSearchParams(window.location.search);
-                  const redirect = query.get('redirect')
-                  window.open(redirect, '_blank');
-                }}
-                 style={{ backgroundColor:"rgba(0, 90, 217, 0.16)"}}  aria-label="add to shopping cart">
-  <NavigateNextIcon />
-</IconButton>
+                  <div className="d-flex">
+
+                  <div className="col-md-4">
+                  <IconButton
+                  className="iconButt"
+                  onClick={() => {
+                    const query = new URLSearchParams(
+                      window.location.search
+                    );
+                    const redirect = query.get("redirect");
+                    if(redirect) {
+                      if (validUrl.isUri(redirect)){
+                    window.open(redirect, "_blank");
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                  }}
+                  style={{ backgroundColor: "rgba(0, 90, 217, 0.16)" }}
+                  aria-label="add to shopping cart"
+                >
+                  <NavigateNextIcon />
+                </IconButton>
+                  </div>
+
+                  <div className="col-md-4">
+                  <IconButton
+                  className="iconButt"
+                  onClick={() => {
+                    const query = new URLSearchParams(
+                      window.location.search
+                    );
+                    const redirect = query.get("redirect1");
+                    if(redirect) {
+                      if (validUrl.isUri(redirect)){
+                    window.open(redirect, "_blank");
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                  }}
+                  style={{ backgroundColor: "rgba(0, 90, 217, 0.16)" }}
+                  aria-label="add to shopping cart"
+                >
+                  <NavigateNextIcon />
+                </IconButton>
+                  </div>
+
+                  <div className="col-md-4">
+                  <IconButton
+                  className="iconButt"
+                  onClick={() => {
+                    const query = new URLSearchParams(
+                      window.location.search
+                    );
+                    const redirect = query.get("redirect2");
+                    if(redirect) {
+                      if (validUrl.isUri(redirect)){
+                    window.open(redirect, "_blank");
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                    }else{
+                      setErr("Enter Valid Url")
+                    }
+                  }}
+                  style={{ backgroundColor: "rgba(0, 90, 217, 0.16)" }}
+                  aria-label="add to shopping cart"
+                >
+                  <NavigateNextIcon />
+                </IconButton>
+                  </div>
+                  
 
 
-                  {/*<span className="padding-bottom--15">
+
+
+                  </div>
+                  <code style={{ backgroundColor: "transparent" }}>
+                  {err}
+                    </code>
+
+
+                    {/*<span className="padding-bottom--15">
                     Sign in to your account
                 </span>*/}
-                  {/*<form id="stripe-login">
+                    {/*<form id="stripe-login">
                     <div className="field padding-bottom--24">
                       <TextField
                         fullWidth
@@ -181,9 +261,7 @@ const Login1 = () => {
                         }}
                       />
                     </div>
-                    <code style={{ backgroundColor: "transparent" }}>
-                    {err}
-                      </code>
+
                     <div className="field padding-bottom--24">
 
                     </div>
@@ -214,7 +292,8 @@ const Login1 = () => {
                       </a>
                     </div>
                       </form>*/}
-                </div>}
+                  </div>
+                }
               </div>
               {/*<div className="footer-link padding-top--24">
             <span>Don't have an account? <a href="/">Sign up</a></span>
